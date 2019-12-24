9cea43cbf045b6f9368b6b6f8c01ed2b28b54223
Thorsten <t.luenborg@googlemail.com>
2019-12-24 13:18:11 +0100

    refactor: use index file for accessing all composables

---
+++ src/composables/index.ts
@@ -0,0 +1,13 @@
+export * from './template-ref-helpers'
+export * from './use-global-focustracker'
+
+export * from './use-focus-group'
+// export * from './use-activedescendant'
+export * from './use-roving-index'
+
+// export * from './use-focustrap'
+
+// export * from './use-arrow-keys'
+export * from './use-events'
+// export * from './use-id-generator'
+// export * from './use-focus-elref'
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-export * from './composables/template-ref-helpers'
-export * from './composables/use-global-focustracker'
-
-export * from './composables/use-focus-group'
-// export * from './composables/use-activedescendant'
-export * from './composables/use-roving-index'
-
-// export * from './composables/use-focustrap'
-
-// export * from './composables/use-arrow-keys'
-export * from './composables/use-events'
-// export * from './composables/use-id-generator'
-// export * from './composables/use-focus-elref'
+export * from './composables'
+export * from './directives'
+export * from './types'
 
 declare module '@vue/composition-api/dist/component/component' {
   interface SetupContext {
