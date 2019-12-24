1db8b82efe726cede625094090b0c9a9b8338325
Thorsten <t.luenborg@googlemail.com>
2019-12-24 13:26:09 +0100

    feat(focusGroup): too much to name, sorry.
    Was sloppy with atomic commits

--- src/composables/use-focus-group.ts
+++ src/composables/use-focus-group.ts
@@ -1,68 +1,63 @@
+import Vue from 'vue'
 import {
-  ref,
-  Ref,
   computed,
   onBeforeUnmount,
-  watch,
+  provide,
+  InjectionKey,
+  inject,
+  getCurrentInstance,
+  onMounted,
 } from '@vue/composition-api'
-import { useKeyIf } from './use-events'
 import { useGlobalFocusTracker } from './use-global-focustracker'
+import { useReturnBehaviour } from './use-return-behaviour'
 import { applyFocus } from '../utils'
+import {
+  TemplateRefs,
+  FocusableElements,
+  ComponentInstance,
+  useFocusGroupOptions,
+} from '../types'
 
-interface useFocusGroupOptions {
-  returnOnEscape?: false | true
-  returnOnUnMount?: false | true
-}
+export const GroupInterfaceKey: InjectionKey<any> = Symbol('GroupInterface')
 
 export function useFocusGroup(
-  templateRefs: Ref<HTMLElement[]>,
-  { returnOnEscape = false, returnOnUnMount = false }: useFocusGroupOptions = {}
+  _elements: TemplateRefs,
+  options: useFocusGroupOptions = {}
 ) {
-  const {
-    prevEl: prevElGlobal,
-    currentEl: currentElGlobal,
-  } = useGlobalFocusTracker()
+  const { integrateWithParentGroup, includeChildComponents } = options
 
+  const { currentEl: currentElGlobal } = useGlobalFocusTracker()
+
+  // complete template Refs with those possibly provided by children
+  const nestedGroups = includeChildComponents && useNestedGroupInterface()
+
+  const elements = computed(() =>
+    !nestedGroups
+      ? _elements.value
+      : _elements.value.flatMap(item => {
+          const id = (item as any)._uid as string
+          if (id && nestedGroups[id]) {
+            return nestedGroups[id]
+          }
+          return item as HTMLElement
+        })
+  )
+
+  // if we ourselves are supposed to integrate with a parent, do it
+  if (integrateWithParentGroup) {
+    integrateWithParent(elements as TemplateRefs) // TODO: fix type
+  }
   const currentTabindex = computed(
-    () =>
-      currentElGlobal.value && templateRefs.value.indexOf(currentElGlobal.value)
+    () => currentElGlobal.value && elements.value.indexOf(currentElGlobal.value)
   )
   const containsFocus = computed(() => currentTabindex.value !== -1)
   const currentEl = computed(() =>
     containsFocus.value ? currentElGlobal.value : null
   )
-  const returnEl = ref<HTMLElement>(null)
-  watch(prevElGlobal, prevEl => {
-    // when we have focus in this focus group
-    // and the previously focused element
-    // is outside of this focus group,
-    // we cache this element as the one we can return focus to.
-    if (
-      containsFocus.value &&
-      prevElGlobal.value &&
-      templateRefs.value.indexOf(prevElGlobal.value) === -1
-    ) {
-      returnEl.value = prevElGlobal.value
-    }
-  })
 
   function setFocusToIndex(index: number) {
-    const el = templateRefs.value[index]
-    el && applyFocus(el)
-  }
-
-  function returnFocus() {
-    const el = returnEl.value
-    el && el.isConnected && applyFocus(el)
-  }
-
-  if (returnOnUnMount) {
-    onBeforeUnmount(returnFocus)
-  }
-  if (returnOnEscape) {
-    useKeyIf(containsFocus, ['Esc'], ((e: KeyboardEvent) => {
-      returnFocus()
-    }) as EventHandlerNonNull)
+    const el = elements.value[index]
+    return el && applyFocus(el) // TODO
   }
 
   return {
@@ -70,9 +65,33 @@ export function useFocusGroup(
     isActive: containsFocus,
     currentEl,
     currentTabindex,
-    returnEl: computed(() => returnEl.value),
     // Fns
-    returnFocus,
     setFocusToIndex,
+    // other
+    ...useReturnBehaviour(containsFocus, options),
   }
+}
+
+type NestedGroups = Record<string, FocusableElements>
+function useNestedGroupInterface() {
+  console.log('provide')
+  const nestedGroups: NestedGroups = {}
+  provide(GroupInterfaceKey, {
+    add: (vm: ComponentInstance, elements: FocusableElements) =>
+      Vue.set(nestedGroups, (vm as any)._uid, elements),
+    remove: (vm: ComponentInstance) =>
+      Vue.delete(nestedGroups, (vm as any)._uid),
+  })
+
+  return nestedGroups
+}
+
+function integrateWithParent(elements: Readonly<TemplateRefs>) {
+  const parentGroupInterface = inject(GroupInterfaceKey)
+  if (!parentGroupInterface) return
+  const vm = getCurrentInstance()
+  onMounted(() => {
+    vm && parentGroupInterface.add(vm, elements)
+  })
+  onBeforeUnmount(() => parentGroupInterface.remove(vm))
 }
