73fbce643aed38666c4e5fce1ed0043f9e677905
Thorsten <t.luenborg@googlemail.com>
2019-12-30 22:50:47 +0100

    examples(Listbox): use new template Ref Fn

--- examples/components/ListBox.vue
+++ examples/components/ListBox.vue
@@ -1,35 +1,50 @@
 <template>
   <div>
     <ul ref="ul" role="listbox">
-      <li class="selectable" ref="el1">Selectable List Item #1</li>
-      <li class="selectable" ref="el2">Selectable List Item #2</li>
-      <li class="selectable" ref="el3">Selectable List Item #3</li>
+      <li class="selectable" v-ref-fn="listboxRefFn">
+        Selectable List Item #1
+      </li>
+      <li class="selectable" v-ref-fn="listboxRefFn">
+        Selectable List Item #2
+      </li>
+      <li class="selectable" v-ref-fn="listboxRefFn">
+        Selectable List Item #3
+      </li>
     </ul>
+    <button @click="show = !show">Toggle</button>
+    <p v-if="show">Toggle Target</p>
   </div>
 </template>
 
 <script lang="ts">
-import { createComponent, computed } from '@vue/composition-api'
+import { createComponent, computed, ref, onUpdated } from '@vue/composition-api'
 import {
   useFocusGroup,
   createTemplateRef,
   useRovingTabIndex,
   createQueryTemplateRef,
+  createTemplateRefFn,
+  refFn,
 } from '../../src'
 
 export default createComponent({
   name: 'ListBox',
+  directives: {
+    refFn,
+  },
   setup(_, { refs }) {
-    // const els = createTemplateRef(refs, ['el1', 'el2', 'el3'])
-    const els = createQueryTemplateRef(
-      computed<HTMLElement>(() => refs.ul as HTMLElement),
-      'li.selectable'
-    )
-    const focusGroup = useFocusGroup(els, {
+    const { elements, fn: listboxRefFn } = createTemplateRefFn()
+    onUpdated(() => console.log(elements.value))
+
+    const focusGroup = useFocusGroup(elements, {
       includeChildComponents: true,
     })
-    useRovingTabIndex(els, focusGroup.isActive)
-    // return { els }
+    useRovingTabIndex(elements, focusGroup.isActive)
+
+    return {
+      listboxRefFn,
+      show: ref(false),
+    }
   },
 })
 </script>
