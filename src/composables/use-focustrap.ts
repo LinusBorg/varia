de8b78217b2c2131dd4c85ef5bb2e7246ae0bf39
Thorsten <t.luenborg@googlemail.com>
2019-12-30 22:48:33 +0100

    feat(focustrap): make usage more imperative as usecase requires

--- src/composables/use-focustrap.ts
+++ src/composables/use-focustrap.ts
@@ -1,36 +1,91 @@
-import { Ref, watch, reactive, computed } from '@vue/composition-api'
+import {
+  Ref,
+  reactive,
+  computed,
+  onMounted,
+  onUnmounted,
+} from '@vue/composition-api'
 import { useEventIf } from './use-events'
 import { applyFocus } from '../utils'
 import { useGlobalFocusTracker } from './use-global-focustracker'
 
 const focusTrapQueue = useFocusTrapQueue()
 
+interface FocusTrapOptions {
+  skipFirst?: boolean
+  skipLast?: boolean
+  activateOnMount?: boolean
+}
 export function useFocusTrap(
   elements: Ref<HTMLElement[]>,
-  conditionRef: Ref<boolean>
+  options: FocusTrapOptions = {}
 ) {
-  const { tabDirection } = useGlobalFocusTracker()
   const id = Symbol('focusGroupId')
+  const activate = () => focusTrapQueue.add(id)
+  const deactivate = () => focusTrapQueue.remove(id)
+  const isTrapActive = computed(() => focusTrapQueue.active.value === id)
+  options.activateOnMount && onMounted(activate)
+  onUnmounted(deactivate)
 
-  watch(conditionRef, isActive => {
-    if (isActive) {
-      focusTrapQueue.add(id)
-    } else {
-      focusTrapQueue.remove(id)
-    }
-  })
-
-  useEventIf(conditionRef, document, 'focusout', event => {
+  // restrict Tab Sequence
+  const { tabDirection } = useGlobalFocusTracker()
+  useEventIf(isTrapActive, document, 'focusout', event => {
     const el = event.target
     const els = elements.value
-    if (els[0] === el && tabDirection.value === 'backward') {
-      const lastEl = els[els.length - 1]
+    const firstEl = els[options.skipFirst ? 1 : 0]
+    const lastEl = els[els.length - (options.skipLast ? 1 : 2)]
+    if (firstEl === el && tabDirection.value === 'backward') {
       applyFocus(lastEl)
-    } else if (els[els.length - 1] === el && tabDirection.value === 'forward') {
-      const firstEl = els[0]
+    } else if (lastEl === el && tabDirection.value === 'forward') {
       applyFocus(firstEl)
     }
   })
+
+  // return focus if - for whatever reason, i.e. a mouse click,
+  // any element not included of the trap's elements has received focus
+  const focusTracker = useGlobalFocusTracker()
+  useEventIf(
+    isTrapActive,
+    document,
+    'focusin',
+    event => {
+      // TODO: does this need a nextTick? Will have to test this ...
+      if (elements.value.indexOf(event.target as HTMLElement) === -1) {
+        focusTracker.prevEl.value && applyFocus(focusTracker.prevEl.value)
+      }
+    },
+    { capture: true }
+  )
+
+  // first element should never have focus
+  // it merely serves as a goalpost
+  if (options.skipFirst) {
+    useEventIf(
+      isTrapActive,
+      computed(() => elements.value[0]),
+      'focusin',
+      () => {
+        applyFocus(elements.value[1])
+      }
+    )
+  }
+  // last element should never have focus
+  if (options.skipLast) {
+    useEventIf(
+      isTrapActive,
+      computed(() => elements.value[elements.value.length - 1]),
+      'focusin',
+      () => {
+        applyFocus(elements.value[elements.value.length - 2])
+      }
+    )
+  }
+
+  return {
+    isActive: computed(() => isTrapActive.value),
+    activate,
+    deactivate,
+  }
 }
 
 function useFocusTrapQueue() {
