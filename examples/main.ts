c58dae7a3c1371f45f12b1e8c2d357d73cbc4c82
Thorsten <t.luenborg@googlemail.com>
2020-04-30 23:11:31 +0200

    chore: remove old /examples folder.
    replaced by /packages/examples

--- examples/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-
-const app = createApp(App).mount('#app')
-
-app.config.productionTip = false
