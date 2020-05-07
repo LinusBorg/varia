606448ff601cb1f0b44557e6ed56273853d66675
Thorsten <t.luenborg@googlemail.com>
2020-05-07 16:21:03 +0200

    remove old roving-index implementation

--- packages/aria-composables/src/composables/index.ts
+++ packages/aria-composables/src/composables/index.ts
@@ -2,7 +2,6 @@ export * from './focus-tracker'
 
 export * from './template-refs'
 export * from './focus-group'
-export * from './roving-index'
 export * from './click-outside'
 export * from './arrow-navigation'
 // export * from './focus-trap'
