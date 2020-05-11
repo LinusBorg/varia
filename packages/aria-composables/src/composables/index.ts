7e61c2ff4a950b1ccc2430121bdca1b3969ae364
Thorsten <t.luenborg@googlemail.com>
2020-05-11 13:38:17 +0200

    feat: get rid of focusgroup composable, not needed

--- packages/aria-composables/src/composables/index.ts
+++ packages/aria-composables/src/composables/index.ts
@@ -1,12 +1,16 @@
+// Focus Tracking
 export * from './focus-tracker'
+export * from './focus-observer'
 
-export * from './template-refs'
-export * from './focus-group'
-export * from './click-outside'
+// Click and Keyboard Events
+export * from './tab-direction'
 export * from './arrow-navigation'
-// export * from './focus-trap'
-
+export * from './click-outside'
 export * from './keys'
 export * from './events'
 
+// State Helpers
 export * from './state'
+
+// Other
+export * from './template-refs'
