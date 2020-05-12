eebf943f8bfb327c7c8f69950f48e7864a78ca96
Thorsten <t.luenborg@googlemail.com>
2020-05-12 21:49:51 +0200

    examples: made App.vue more accessible

--- packages/examples/src/App.vue
+++ packages/examples/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/buttons">Buttons</router-link> |
-      <router-link to="/disclosures">Disclosures</router-link> |
-      <router-link to="/accordions">Accordions</router-link> |
-      <router-link to="/popovers">Popovers</router-link> |
-      <router-link to="/focustraps">FocusTraps</router-link> |
-      <router-link to="/tabs">Tabs</router-link>
-    </div>
+  <div id="container">
+    <nav id="nav" aria-labelledby="main-nav-label">
+      <span id="main-nav-label" data-varia-visually-hidden="true"
+        >main navigation</span
+      >
+      <router-link to="/">Home</router-link>
+      <router-link to="/buttons" :aria-current="isCurrentRoute('Buttons')">
+        Buttons
+      </router-link>
+      <router-link
+        to="/disclosures"
+        :aria-current="isCurrentRoute('Disclosures')"
+      >
+        Disclosures
+      </router-link>
+      <router-link
+        to="/accordions"
+        :aria-current="isCurrentRoute('Accordions')"
+      >
+        Accordions
+      </router-link>
+      <router-link to="/popovers" :aria-current="isCurrentRoute('Popovers')">
+        Popovers
+      </router-link>
+      <router-link to="/focustraps" :aria-current="isCurrentRoute('FousTraps')">
+        FocusTraps
+      </router-link>
+      <router-link to="/tabs" :aria-current="isCurrentRoute('Tabs')">
+        Tabs
+      </router-link>
+    </nav>
     <main id="main" class="text-left mx-auto">
       <router-view />
     </main>
@@ -17,28 +38,35 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import { useRoute } from 'vue-router'
 export default defineComponent({
   name: 'App',
+  setup() {
+    const route = useRoute()
+    return {
+      isCurrentRoute: (name: string) =>
+        route.name === name ? 'page' : undefined,
+    }
+  },
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#container {
+  display: flex;
+  margin: 0;
+  min-height: 100vh;
 }
 
 #nav {
   padding: 30px;
+  flex: 0 0 250px;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  display: block;
 }
 
 #nav a.router-link-exact-active {
@@ -46,6 +74,8 @@ export default defineComponent({
 }
 
 #main {
+  margin: 30px;
   max-width: 980px;
+  flex: 1 1 auto;
 }
 </style>
