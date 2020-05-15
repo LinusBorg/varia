e488151478a679752782417a69687cb3b0d3b14f
Thorsten <t.luenborg@googlemail.com>
2020-05-15 23:07:55 +0200

    fix(SkipToContent):  weird reactivit bug.

--- packages/aria-widgets/src/SkipToContent/index.ts
+++ packages/aria-widgets/src/SkipToContent/index.ts
@@ -1,24 +1,22 @@
 import { ref, defineComponent, h, watch } from 'vue'
 import {
-  MaybeRef,
   TemplRef,
   wrapProp,
   useElementFocusObserver,
 } from 'vue-aria-composables'
 import { useClickable } from '../Clickable'
 
-export function useSkipToContent(_el?: TemplRef) {
-  const el = _el ?? ref()
+// export function useSkipToContent(_el?: TemplRef) {
+//   const el = _el ?? ref()
 
-  const { hasFocus } = useElementFocusObserver(el)
-
-  return {
-    hasFocus: hasFocus,
-    attributes: {
-      ref: el,
-    },
-  }
-}
+//   watch(hasFocus, () => void 0)
+//   return {
+//     hasFocus: hasFocus,
+//     attributes: {
+//       ref: el,
+//     },
+//   }
+// }
 
 export const SkipToContent = defineComponent({
   name: 'SkipToContent',
@@ -30,20 +28,21 @@ export const SkipToContent = defineComponent({
     },
   },
   setup(props, { slots }) {
-    const id = wrapProp(props, 'contentId')
     const el: TemplRef = ref()
     const clickableAttrs = useClickable({}, el)
-    const { hasFocus, attributes } = useSkipToContent(el)
-
+    const { hasFocus } = useElementFocusObserver(el)
+    // This watch is required to work around a strange bug.
+    watch(hasFocus, () => void 0)
     return () => {
       const tag = props.tag ? props.tag.toLowerCase() : 'a'
       return h(
         'a',
         {
           href: props.contentId,
-          'data-varia-visually-hidden': !hasFocus.value,
+          'data-varia-visually-hidden': hasFocus.value ? undefined : true,
           ...(tag === 'a' ? {} : clickableAttrs.value),
-          ...attributes,
+          //...attributes,
+          ref: el,
         },
         slots.default?.()
       )
