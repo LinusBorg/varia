b1aacb2f9e8d90f8d2fa60af61ddcc4e50511124
Thorsten <t.luenborg@googlemail.com>
2020-05-21 10:59:13 +0200

    fix(TabDirection): reset tabDir when user clicks a focusable element.

--- packages/aria-composables/src/composables/tab-direction.ts
+++ packages/aria-composables/src/composables/tab-direction.ts
@@ -1,6 +1,7 @@
 import { ref, provide, computed, inject, InjectionKey, readonly } from 'vue'
 import { useEvent } from './events'
 import { TabDirection } from '../types'
+import { TABBABLE_ELS } from '../utils/focusable-elements'
 
 export const tabDirectionKey: InjectionKey<TabDirection> = Symbol(
   'TabDirectionKey'
@@ -11,6 +12,12 @@ export const tabDirection = ref<'backward' | 'forward' | undefined>()
 useEvent(document, 'keydown', ((event: KeyboardEvent) => {
   if (event.key === 'Tab') {
     tabDirection.value = event.shiftKey ? 'backward' : 'forward'
+  }
+}) as EventListener)
+
+useEvent(document, 'click', ((event: MouseEvent) => {
+  if ((event.target as Element).matches(TABBABLE_ELS)) {
+    setTimeout(() => (tabDirection.value = undefined), 0)
   }
 }) as EventListener)
 
