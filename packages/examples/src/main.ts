c79b74e627d94e7fde1dcd1046378c14efec099c
Thorsten <t.luenborg@googlemail.com>
2020-05-21 14:33:22 +0200

    fiy(examples): import varia styles seperately

--- packages/examples/src/main.ts
+++ packages/examples/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { install as plugin } from '@varia/widgets'
+import '@varia/widgets/dist/index.css'
 import './assets/tailwind.css'
 createApp(App)
   .use(router)
